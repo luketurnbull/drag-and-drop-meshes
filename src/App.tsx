588dd9b06726f237e817840196ec11d60f269dfc
Luke David Turnbull <lukedturnbull@gmail.com>
2025-02-01 20:13:10 +1100

    Created basic dragging functionality
    - Created button that is draggable
    - Track when button is hovered over canvas section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Scene from "./components/scene";
+import { useRef } from "react";
 
 export default function App() {
+  const dragItem = useRef<HTMLButtonElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const handleDragStart = (e: React.DragEvent<HTMLButtonElement>) => {
+    dragItem.current = e.currentTarget;
+  };
+
+  const handleDragEnd = (e: React.DragEvent<HTMLButtonElement>) => {
+    dragItem.current = null;
+  };
+
+  const handleDragEnterCanvas = (e: React.DragEvent<HTMLCanvasElement>) => {
+    if (dragItem.current) {
+      dragItem.current.style.opacity = "0.5";
+    }
+  };
+
+  const handleDragLeaveCanvas = (e: React.DragEvent<HTMLCanvasElement>) => {
+    if (dragItem.current) {
+      dragItem.current.style.opacity = "1";
+    }
+  };
+
   return (
     <main className="grid h-screen w-full grid-cols-[300px_1fr]">
       <aside className="bg-gray-100 p-4">
         <h2>Sidebar</h2>
+
+        <button
+          draggable
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+        >
+          Drag me
+        </button>
       </aside>
-      <section className="w-full h-full min-w-0">
+      <section
+        className="w-full h-full min-w-0"
+        onDragEnter={handleDragEnterCanvas}
+        onDragLeave={handleDragLeaveCanvas}
+      >
         <Canvas
           shadows
           camera={{
@@ -17,8 +53,10 @@ export default function App() {
             far: 200,
             position: [6, 4, 8],
           }}
+          ref={canvasRef}
         >
           <OrbitControls />
+
           <ambientLight intensity={0.5} />
           <directionalLight position={[10, 10, 10]} />
 

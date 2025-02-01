ec5d0f3ec826c32079ca3be827ff59a13d18f864
Luke David Turnbull <lukedturnbull@gmail.com>
2025-02-01 22:19:29 +1100

    Clean up code
    - Moved GEOMETRY_MAP into shared space

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,23 +1,8 @@
 import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
-import {
-  BoxGeometry,
-  BufferGeometry,
-  CylinderGeometry,
-  Mesh,
-  SphereGeometry,
-  TorusKnotGeometry,
-  Vector2,
-  Vector3,
-} from "three";
+import { BufferGeometry, Mesh, Vector3 } from "three";
 import { MeshType } from "../utils/types";
-
-const GEOMETRY_MAP: Record<MeshType, BufferGeometry> = {
-  cube: new BoxGeometry(1, 1, 1),
-  sphere: new SphereGeometry(0.5, 100, 100),
-  cylinder: new CylinderGeometry(0.5, 0.5, 1, 10, 10),
-  "torus-knot": new TorusKnotGeometry(0.5, 0.25, 100, 8),
-};
+import { GEOMETRY_MAP } from "../utils/meshes";
 
 export default function Scene({
   isOverCanvas,
@@ -36,7 +21,8 @@ export default function Scene({
 
   const ghostMesh = useMemo(() => {
     if (isOverCanvas && dragItem) {
-      const geometry = GEOMETRY_MAP[dragItem];
+      const geometry: BufferGeometry = GEOMETRY_MAP[dragItem];
+
       return {
         type: dragItem,
         position: new Vector3(0, 0, 0),
--- src/utils/meshes.ts
+++ src/utils/meshes.ts
@@ -1,3 +1,17 @@
+import {
+  BoxGeometry,
+  BufferGeometry,
+  CylinderGeometry,
+  SphereGeometry,
+  TorusKnotGeometry,
+} from "three";
 import { MeshType } from "./types";
 
 export const MESHES: MeshType[] = ["cube", "sphere", "cylinder", "torus-knot"];
+
+export const GEOMETRY_MAP: Record<MeshType, BufferGeometry> = {
+  cube: new BoxGeometry(1, 1, 1),
+  sphere: new SphereGeometry(0.5, 100, 100),
+  cylinder: new CylinderGeometry(0.5, 0.5, 1, 10, 10),
+  "torus-knot": new TorusKnotGeometry(0.5, 0.25, 100, 8),
+};
